a748dae483c82b7692037d7f4a084d906e5797ed
Кочеткова Екатерина <94628140+EkaKochetkova@users.noreply.github.com>
2022-02-27 22:42:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <cmath> 
 
 
 bool checkPrime(uint64_t value) {
@@ -18,9 +17,9 @@ uint64_t nPrime(uint64_t n) {
     bool flag;
     uint64_t k = 0;
     uint64_t number;
-    for (uint64_t i = 2; i <= 999999999; ++i) {
+    for (uint64_t i = 2; i <= 999999999999; ++i) {
         flag = true;
-        for (uint64_t j = 2; j <= sqrt(i); ++j) {
+        for (uint64_t j = 2; j*j <= i; ++j) {
             if (i % j == 0) {
                 flag = false;
             }
@@ -37,9 +36,9 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-for (uint64_t i = value + 1; i <= value + 9999999; ++i) {
+for (uint64_t i = value + 1; i <= value + 999999999999; ++i) {
         bool flag = true;
-        for (uint64_t j = 2; j <= sqrt(i); ++j) {
+        for (uint64_t j = 2; j*j <= i; ++j) {
             if (i % j == 0) {
                 flag = false;
             }
@@ -55,7 +54,7 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
         bool flag = true;
-        for (uint64_t j=2; j <= sqrt(i); ++j) {
+        for (uint64_t j=2; j*j <= i; ++j) {
             if (i % j == 0) {
                 flag = false;
             }
