81ad23b6987c23e0d3daed416c92c61f1b2dd40f
Кочеткова Екатерина <94628140+EkaKochetkova@users.noreply.github.com>
2022-02-27 20:18:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,64 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+ bool flag = true;
+    for (uint64_t i = 2; i < (value / 2 + 1); ++i) {
+        if (value % i == 0) {
+            flag = false;
+        }
+    }
+    return flag;
+   }
+    return flag;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+ bool flag;
+    uint64_t k = 0;
+    uint64_t number;
+    for (uint64_t i = 2; i <= 999999999; ++i) {
+        flag = true;
+        for (uint64_t j = 2; j <= sqrt(i); ++j) {
+            if (i % j == 0) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            k += 1;
+            number = i;
+        }
+        if (k == n) {
+            return number;
+        }
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+for (uint64_t i = value + 1; i <= value + 9999999; ++i) {
+        bool flag = true;
+        for (uint64_t j = 2; j <= sqrt(i); ++j) {
+            if (i % j == 0) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            return i;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; ++i) {
+        bool flag = true;
+        for (uint64_t j=2; j <= sqrt(i); ++j) {
+            if (i % j == 0) {
+                flag = false;
+            }
+        }
+        if (flag) {
+           sum+=i;       
+        }
+    }
+    return sum;
 }
